56a16519b245e4dfe41b6e6bb9371ff5686a5b7d
Tarek Dinkespiler <tarek.dinkespiler@gmail.com>
2024-10-05 11:33:56 +0200

    fix controller test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get Home" do
-    get pages_Home_url
+    get root_path
     assert_response :success
   end
 end
